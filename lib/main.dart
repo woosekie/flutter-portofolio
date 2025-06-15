56cc3792eb90b9f8a7c53622fc6f848fccba00b2
Rizky Nuansa <rizkynuansa2@mail.com>
2025-06-15 19:52:39 +0700

    Add icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Portfolio',
+      title: 'Rizky’s Portofolio',
       initialRoute: AppRoutes.home,
       getPages: AppPages.routes,
     );
